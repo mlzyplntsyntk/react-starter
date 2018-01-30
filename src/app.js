c8b01394d6690dad56692365167aad1aff978405
Emir Emre SARBAY <emre@sarbay.com>
2018-01-30 22:07:04 +0300

    asd

---
+++ src/app.js
@@ -0,0 +1,11 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+class App extends React.Component {
+	render() {
+		return (
+			<div>aasd</div>
+		);
+	}
+}
+ReactDOM.render(<App />, document.getElementById('app'));
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+var WrapperPlugin = require('wrapper-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+
+module.exports = {
+
+  entry: './src/app.js',
+  output: {
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /.js?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  },
+  plugins: [
+    // strict mode for the whole bundle,
+    //new UglifyJsPlugin(),
+    new WrapperPlugin({
+      test: /\.js$/, // only wrap output of bundle files with '.js' extension
+      header: 'var appLoader = function() {\n',
+      footer: '\n};'
+    })
+  ]
+};
